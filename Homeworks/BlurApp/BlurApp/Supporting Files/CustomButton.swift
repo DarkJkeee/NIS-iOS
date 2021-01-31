2ae87c3f93bac871a64cc67fcda4e4bf09302280
Gleb <burshtejn2000@mail.ru>
2021-01-31 16:34:10 +0300

    BlurApp

--- Homeworks/BlurApp/BlurApp/Supporting Files/CustomButton.swift
+++ Homeworks/BlurApp/BlurApp/Supporting Files/CustomButton.swift
@@ -7,14 +7,14 @@
 
 import UIKit
 
+@IBDesignable
 class CustomButton: UIButton {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    @IBInspectable var cornerRadius: CGFloat = 0.0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+            layer.masksToBounds = cornerRadius > 0
+        }
     }
-    */
 
 }
